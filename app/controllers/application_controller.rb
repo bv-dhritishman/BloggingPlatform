62466796537adff2e595225c35ea6602857d1dd6
bv-dhritishman <dhritishman@blogvault.com>
2020-07-14 16:48:26 +0530

    Added Users and Sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_author  
+  
+  def current_author
+    if session[:author_id]
+      @current_author ||= Author.find(session[:author_id])
+    else
+      @current_author = nil
+    end
+  end
 end
